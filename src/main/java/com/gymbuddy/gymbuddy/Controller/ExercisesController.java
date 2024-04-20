a360ef955510e6f0d5138cb57482ba98f0524b89
Jeremy Guillen <guillenjeremy26@gmail.com>
2024-04-20 13:22:49 -0600

    added put method in exercise controller

--- src/main/java/com/gymbuddy/gymbuddy/Controller/ExercisesController.java
+++ src/main/java/com/gymbuddy/gymbuddy/Controller/ExercisesController.java
@@ -7,11 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 @CrossOrigin("*")
 public class ExercisesController {
   private final ExericsesRepository exericsesRepository;
+  Logger logger = Logger.getLogger(getClass().getName());
 
   @Autowired
   public ExercisesController(ExericsesRepository exericsesRepository) {
@@ -25,8 +28,27 @@ public class ExercisesController {
 
   @GetMapping("exercises/{exerciseId}")
   public ResponseEntity<Exercises> getExercises(@PathVariable String exerciseId) {
-    System.out.println(exerciseId);
     Optional<Exercises> exercise = exericsesRepository.findById(Long.parseLong(exerciseId));
     return exercise.map(exercises -> ResponseEntity.ok().body(exercises)).orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @PutMapping("exercises/{exerciseId}")
+  public ResponseEntity<Exercises> updateExercise(@PathVariable String exerciseId, @RequestBody Exercises exercise) {
+    try {
+      logger.log(Level.INFO, "Starting update exercise by id with exercise id: {}", exerciseId);
+      Optional<Exercises> exerciseToUpdate = exericsesRepository.findById(Long.parseLong(exerciseId));
+      if (exerciseToUpdate.isEmpty()) {
+        return ResponseEntity.notFound().build();
+      }
+      exerciseToUpdate.get().setName(exercise.getName());
+      exerciseToUpdate.get().setDescription(exercise.getDescription());
+      exerciseToUpdate.get().setVideUrl(exercise.getVideUrl());
+      Exercises savedExercise = exericsesRepository.save(exerciseToUpdate.get());
+      return ResponseEntity.ok().body(savedExercise);
+    } catch (Exception e) {
+      logger.severe(e.getMessage());
+      return ResponseEntity.internalServerError().build();
+    }
+
   }
 }
